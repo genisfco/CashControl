1d863dc8880f4cf7969cac42bbc146810dd03c0b
Youapin <laurindo.ytbr@gmail.com>
2023-06-05 20:18:53 -0300

    Identei/ Estilizei extract,menu e registers

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -8,13 +8,6 @@
     <title>Menu</title>
 </head>
 <body>
-    <div class="justify-end sm:col-span-1" style="background-color: aqua">
-    <div class="sm:col-span-2 sm:col-start-1 justify-end">
-        <div class="mt-2">
-            <button type="submit" class="flex w-36 h-16 my-26 ml-24 rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white text-center shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Voltar</button>
-        </div>
-    </div>
-</div>
     <div class="sm:col-span-2 sm:col-start-1">
         <div class="mt-2">
             <button type="submit" class="flex w-96 h-36 justify-start my-13 ml-24 rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white text-center shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Cadastrar ganho</button>
--- resources/views/registerAmountSpent.blade.php
+++ resources/views/registerAmountSpent.blade.php
@@ -11,37 +11,37 @@
     <form>
         <div class="border-b border-gray-900/10 pb-12">
             <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                <div class="sm:col-span-2">
+                <div class="sm:col-span-2 sm:col-start-3">
                     <label for="first-name" class="block text-sm font-medium leading-6 text-gray-900">Nome do gasto</label>
                 <div class="mt-2">
                     <input type="text" name="user-name" id="user-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                 </div>
             </div>
-            <div class="sm:col-span-2 sm:col-start-1">
+            <div class="sm:col-span-2 sm:col-start-3">
                 <label for="city" class="block text-sm font-medium leading-6 text-gray-900">Categoria</label>
                 <div class="mt-2">
                     <input type="text" name="password" id="password" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                 </div>
             </div>
-            <div class="sm:col-span-2 sm:col-start-1">
+            <div class="sm:col-span-2 sm:col-start-3">
                 <label for="city" class="block text-sm font-medium leading-6 text-gray-900">Data</label>
                 <div class="mt-2">
                     <input type="text" name="password" id="password" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                 </div>
             </div>
-            <div class="sm:col-span-2 sm:col-start-1">
+            <div class="sm:col-span-2 sm:col-start-3">
                 <label for="city" class="block text-sm font-medium leading-6 text-gray-900">Valor</label>
                 <div class="mt-2">
                     <input type="text" name="password" id="password" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                 </div>
             </div>
-            <div class="sm:col-span-2 sm:col-start-1">
+            <div class="sm:col-span-2 sm:col-start-3">
                 <label for="city" class="block text-sm font-medium leading-6 text-gray-900">Provem</label>
                 <div class="mt-2">
                     <input type="text" name="password" id="password" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                 </div>
             </div>
-            <div class="sm:col-span-2 sm:col-start-1">
+            <div class="sm:col-span-2 sm:col-start-2">
                 <label for="city" class="block text-sm font-medium leading-6 text-gray-900">Recorrente</label>
             </div>
             <button
@@ -54,7 +54,7 @@
                 class="inline-block rounded bg-primary-accent-200 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-primary-700 transition duration-150 ease-in-out hover:bg-primary-accent-100 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200">
                 Não
             </button>
-            <div class="sm:col-span-2 sm:col-start-1">
+            <div class="sm:col-span-2 sm:col-start-3">
                 <div class="mt-2">
                     <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Cadastrar</button>
                 </div>
